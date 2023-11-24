de9bb27987fc804b9952a15910b33e77c3d8acf1
Ncrafter <119175392+Ncrafter@users.noreply.github.com>
2023-11-24 14:15:42 +0100

    300

--- main.ts
+++ main.ts
@@ -10,10 +10,22 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
         led.toggle(2, 4 - Schwierigkeit)
     }
 })
+let LEDy = 0
+let LEDx = 0
 let Schwierigkeit = 0
 Schwierigkeit = 1
 let Spielstart = false
 led.plot(2, 4)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.clearScreen()
+        Spielstart = true
+        music.playMelody("C D E F G A B C5 ", 250)
+        basic.pause(100)
+    }
+    if (Spielstart == true) {
+        led.unplot(LEDx, LEDy)
+        LEDx = Math.map(input.acceleration(Dimension.X) * Schwierigkeit, -1023, 1023, 0, 4)
+        LEDy = Math.map(input.acceleration(Dimension.Y) * Schwierigkeit, -1023, 1023, 0, 4)
+    }
 })
